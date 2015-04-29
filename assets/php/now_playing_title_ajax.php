067e36d5085675e017c2fbf89ef489ebaf4d770f
Cozza38 <cozza380@gmail.com>
2015-04-29 17:50:19 +1000

    Show "Recently Viewed" if looking at trakt

--- assets/php/now_playing_title_ajax.php
+++ assets/php/now_playing_title_ajax.php
@@ -7,23 +7,21 @@
 	// is a special scrollable div and we don't want the title scrolling with it.
 	// You will only notice the scrolling feature when there are multiple
 	// shows being watched at the same time.
-	
+
 	$network = getNetwork();
-$plexSessionXML = simplexml_load_file($network . ':' . $plex_port . '/status/sessions/?X-Plex-Token=' . $plexToken);
+	$plexSessionXML = simplexml_load_file($network . ':' . $plex_port . '/status/sessions/?X-Plex-Token=' . $plexToken);
 
 	// See if Plex Media Server is online and how many people are watching.
-	if (!$plexSessionXML):
-		// If Plex Media Server is offline.
-		// TODO clean this up $title = 'Recently Viewed';
-    $title = 'Plex Offline';
-	else:
+	if (!$plexSessionXML) {
+    	$title = 'Recently Viewed';
+	} else {
 		// If Plex Media Server is online.
-		if (count($plexSessionXML->Video) == 0):
+		if (count($plexSessionXML->Video) == 0) {
 			$title = 'Recently Added';
-		else:
+		} else {
 			$title = 'Now Playing';
-		endif;
-	endif;
+		}
+	}
 
 	echo '<h1 class="exoextralight">'.$title.'</h1>';
 	echo '<hr>';
